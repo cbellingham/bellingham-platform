c8542edff5e3231143717e5c197a5ae5695564e8
cbellingham <103128901+cbellingham@users.noreply.github.com>
2025-07-03 17:43:40 +0100

    Shift logo left on login

--- bellingham-frontend/src/components/Logo.jsx
+++ bellingham-frontend/src/components/Logo.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import logoImage from "../assets/login.png";
 
-const Logo = () => (
-    <img
-        src={logoImage}
-        alt="Logo"
-        className="fixed bottom-4 right-4 w-24 h-24 pointer-events-none"
-    />
-);
+const Logo = () => {
+    const style =
+        window.location.pathname === "/login"
+            ? { right: "calc(1rem + 10px)" }
+            : {};
+
+    return (
+        <img
+            src={logoImage}
+            alt="Logo"
+            className="fixed bottom-4 right-4 w-24 h-24 pointer-events-none"
+            style={style}
+        />
+    );
+};
 
 export default Logo;
